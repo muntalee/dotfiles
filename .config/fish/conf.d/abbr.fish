e26a803680015e705f28df65fe62c9262ea136fb
Munta Islam <islammuntasirul@gmail.com>
2024-09-23 11:01:28 -0400

    fish acquired

---
+++ .config/fish/conf.d/abbr.fish
@@ -0,0 +1,14 @@
+abbr vim "nvim"
+
+if type -q eza
+    abbr --add -g ls 'eza -a'
+    abbr --add -g ll 'eza -lag --header'
+end
+
+if type -q bat
+    abbr --add -g cat 'bat'
+end
+
+if type -q trash
+    abbr --add -g rm 'trash'
+end
---
+++ .config/fish/config.fish
@@ -0,0 +1,26 @@
+starship init fish | source
+set -gx EDITOR nvim
+
+# Setup brew
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
+# Clear line on CTRL + C
+# Sometimes it still doesn't work well enough on node.js scripts :(
+bind --preset \cC 'cancel-commandline'
+
+# Auto-switch nvm version on cd
+# Requires a ~/.node-version file with a valid node version
+# https://github.com/jorgebucaran/nvm.fish/pull/186
+if type -q nvm
+    function __nvm_auto --on-variable PWD
+        nvm use --silent 2>/dev/null # Comment out the silent flag for debugging
+    end
+    __nvm_auto
+end
+
+# Pyenv setup
+# Requires `brew install pyenv`
+if type -q pyenv
+    status --is-interactive; and source (pyenv init -|psub)
+end
+
