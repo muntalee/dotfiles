aa6a037d823a8fe178b6227fd3c017a7aada6edd
Muntasirul Islam <islammuntasirul@gmail.com>
2024-10-11 15:28:30 -0400

    powershell time

---
+++ .config/powershell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,12 @@
+Invoke-Expression (&starship init powershell)
+Set-Alias -Name vim -Value nvim
+Set-Alias ll ls
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program Files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program Files\Git\usr\bin\less.exe'
+
+function which ($command) {
+  Get-Command -Name $command -ErrorAction SilentlyContinue |
+    Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
